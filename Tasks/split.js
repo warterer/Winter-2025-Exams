62990b5d584679c3d554f7bbe0ae2a7c3f2a3452
areutskyi <artemik.tamik@gmail.com>
2025-01-14 23:26:22 +0200

    added 'use strict'

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,6 +1,8 @@
 // Refactor following solution
 // Split an array into two parts
 
+'use strict';
+
 let split_Array = (index, array) => {
   begin = array.slice(0, index);
   const len = array.length;
