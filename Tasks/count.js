0b3e85e4787b747fd614585906334753dba35eba
areutskyi <artemik.tamik@gmail.com>
2025-01-14 22:09:25 +0200

    added const/let and forEach replaced with for of

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,13 +3,15 @@
 
 'use strict';
 
-count = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+    if (typeof value === "number") {
+      sum += value;
+    }
+  }
   return sum;
 };
 
