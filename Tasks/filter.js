3734e3f60445882bbfa81b721189b39e2c57c5cd
areutskyi <artemik.tamik@gmail.com>
2025-01-14 22:46:07 +0200

    added const and renamed variables

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,16 +3,16 @@
 
 'use strict';
 
-Filter = (T, t) => {
+const filterByType = (array, typeName) => {
   remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
+  for (C of array) {
+    x = array.indexOf(C);
+    if (typeof array[x] !== typeName) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (x of remove) array.splice(x, 1);
+  return array;
 };
 
-module.exports = Filter;
+module.exports = filterByType;
