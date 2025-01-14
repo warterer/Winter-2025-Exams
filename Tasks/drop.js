0ccdc4e4b6aebb093393499db05544122ddc62cf
areutskyi <artemik.tamik@gmail.com>
2025-01-14 22:41:13 +0200

    forEach replaced with for of and formatted

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -5,7 +5,11 @@
 
 const drop = (dictionary, ...keysToRemove) => {
   const dictKeys = Object.keys(dictionary);
-  
+  for (const key of keysToRemove) {
+    if (dictKeys.includes(key)) {
+      delete dictionary[key];
+    }
+  }
   return dictionary;
 };
 
