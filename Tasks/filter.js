76428bd03ee6d34189e672c549380d292a1ffce8
areutskyi <artemik.tamik@gmail.com>
2025-01-14 22:50:55 +0200

    simplified logic

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -4,15 +4,13 @@
 'use strict';
 
 const filterByType = (array, typeName) => {
-  remove = [];
-  for (C of array) {
-    x = array.indexOf(C);
-    if (typeof array[x] !== typeName) {
-      remove.unshift(x);
+  const result = [];
+  for (const item of array) {
+    if (typeof item === typeName) {
+      result.push(item);
     }
   }
-  for (x of remove) array.splice(x, 1);
-  return array;
+  return result;
 };
 
 module.exports = filterByType;
