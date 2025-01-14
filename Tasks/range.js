4d34bc55d8fb39006ba26f41eb1be2a5adb64932
areutskyi <artemik.tamik@gmail.com>
2025-01-14 23:06:46 +0200

    formatted

--- Tasks/range.js
+++ Tasks/range.js
@@ -5,13 +5,11 @@
 
 const generateArray = (...range) => {
   const [from, to] = range;
-  if(to < from)
-  {
+  if (to < from) {
     return [];
   }
   const result = [];
-  for(let i = from; i <= to; i++)
-  {
+  for (let i = from; i <= to; i++) {
     result.push(i);
   }
   return result;
